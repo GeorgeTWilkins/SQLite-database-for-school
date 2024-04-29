0b2b71576290a5d46fc49aea0604035ed3094c68
George <22180@burnside.school.nz>
2024-04-29 20:53:42 +1200

    formatting and first function

--- fighters.py
+++ fighters.py
@@ -1,12 +1,27 @@
+#Database application - George Wilkins - Following tutorial
+#Imports
+
 import sqlite3
 
-db = sqlite3.connect("fighters.db")
-cursor = db.cursor()
-sql = "SELECT * FROM fighter;"
-cursor.execute(sql)
-results = cursor.fetchall()
-print(results)
+#constants and variables
 
-db.close(3)
+DATABASE = "fighters.db"
+
+
+
+#functions
+def print_all_aircraft():
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM fighter;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    print(results)
+    db.close()
+
+
+
+#main code
+print_all_aircraft()
 
 
