384c6124821bbaf3840de7c16f556dea20aac243
George <22180@burnside.school.nz>
2024-04-29 21:38:15 +1200

    added more functions to sort data

--- fighters.py
+++ fighters.py
@@ -38,14 +38,63 @@ def print_all_aircraft_sorted_speed_desc():
     #loop finsihed now
     db.close()
 
+def print_all_aircraft_sorted_climb_rate_desc():
+    '''print all the aircraft data neatly, ordered by climb rate desc'''
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM fighters ORDER BY climbrate DESC;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through results first
+    print("Name                          Speed   Max_g Climb Range Payload")
+    for fighter in results:
+        print(f"{fighter[1]:<30}{fighter[2]:<8}{fighter[3]:<6}{fighter[4]:<6}{fighter[5]:<6}{fighter[6]:<6}")
+    #loop finsihed now
+    db.close()
+
+def print_all_aircraft_sorted_range_desc():
+    '''print all the aircraft data neatly, ordered by range desc'''
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM fighters ORDER BY range DESC;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through results first
+    print("Name                          Speed   Max_g Climb Range Payload")
+    for fighter in results:
+        print(f"{fighter[1]:<30}{fighter[2]:<8}{fighter[3]:<6}{fighter[4]:<6}{fighter[5]:<6}{fighter[6]:<6}")
+    #loop finsihed now
+    db.close()
+
+def print_all_aircraft_sorted_payload_desc():
+    '''print all the aircraft data neatly, ordered by payload desc'''
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM fighters ORDER BY payload DESC;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through results first
+    print("Name                          Speed   Max_g Climb Range Payload")
+    for fighter in results:
+        print(f"{fighter[1]:<30}{fighter[2]:<8}{fighter[3]:<6}{fighter[4]:<6}{fighter[5]:<6}{fighter[6]:<6}")
+    #loop finsihed now
+    db.close()
+
+
 
 #main code
 while True:
-    user_input = input("\nWhat would you like to do.\n1. Print all aircraft\n2. Print all aircraft ordered by speed desc\n0. Exit\n")
+    user_input = input("\nWhat would you like to do.\n1. Print all aircraft\n2. Print all aircraft ordered by speed desc\n3. Print all aircraft ordered by climb ratedesc\n4. Print all aircraft ordered by range desc\n5. Print all ordered by payload desc\n0. Exit\n")
     if user_input == "1":
         print_all_aircraft()
     if user_input == "2":
         print_all_aircraft_sorted_speed_desc()
+    if user_input == "3":
+        print_all_aircraft_sorted_climb_rate_desc()
+    if user_input == "4":
+        print_all_aircraft_sorted_range_desc()
+    if user_input == "5":
+        print_all_aircraft_sorted_payload_desc()
     if user_input == "0":
         break
     else:
